8637f633adf5ac2f8744d7ab712359bb47f1cb73
Gaela <ag.independant@gmail.com>
2021-10-02 12:18:29 +0200

    Menu et page crée , si probleme me contacter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
+/*   NAVIGATION   */
+/*=======================================================*/
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
   burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
 
+    /*   ANIMATION   */
     navLinks.forEach((link, index) => {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+      link.style.animation = `navLink 0.8s ease forwards ${index / 4}s`; // On divise la transition par 4
     });
   });
 };
-
+console.log(window);
 navSlide();
+/*   ANIMATION BURGER  */
+
+// /*   STICKY   */
+// window.addEventListener("scroll", function () {
+//   let mainNav = document.querySelector("#mainNav");
+
+//   if (window.pageYOffset > 0) {
+//     mainNav.classList.add("is-sticky");
+//   } else {
+//     mainNav.classList.remove("is-sticky");
+//   }
+// });
